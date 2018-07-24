198fbab71ae255a03997c129bb1dacf7e116b8b6
Rajab Mkomwa <rajeyw@gmail.com>
2018-07-24 13:27:02 +0300

    Created library for chart

---
+++ projects/ngx-dhis2-chart/src/public_api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-dhis2-chart
+ */
+
+export * from './lib/ngx-dhis2-chart.module';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgxDhis2ChartModule } from 'projects/ngx-dhis2-chart/src/public_api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, NgxDhis2ChartModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
